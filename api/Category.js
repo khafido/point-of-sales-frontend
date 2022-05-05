fcc3a1cbeb9832c8b9e365f2ac326950737b0a38
Alvian N R <42503273+lambnet@users.noreply.github.com>
2022-05-05 13:16:46 +0800

    category api FE wrapper

---
+++ api/Category.js
@@ -0,0 +1,58 @@
+import apiClient from "api";
+
+const url = '/category'
+
+export async function listCategory(active, page, size){
+    return apiClient
+        .get(url, {
+            params: {
+                active,
+                page,
+                size
+        }})
+        .then(response => {
+            if(response){
+                return response.data
+            }
+            return false
+        })
+        .catch(err => console.log(err))
+}
+
+export async function addCategory(name){
+    return apiClient
+        .post(url, {
+            name
+        })
+        .then(response => {
+            if(response){
+                return response.data
+            }
+            return response.status
+        })
+        .catch(err => console.log(err))
+}
+
+export async function updateCategory(id, {name}){
+    return apiClient
+        .put(`${url}/${id}`, {name})
+        .then(response => {
+            if(response){
+                return response.data
+            }
+            return false
+        })
+        .catch(err => console.log(err))
+}
+
+export async function deleteCategor(id){
+    return apiClient
+        .patch(`${url}/${id}`)
+        .then(response => {
+            if(response){
+                return response.data
+            }
+            return false
+        })
+        .catch(err => console.log(err))
+}
