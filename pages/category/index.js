e8b6f5ef1228ede566ae92e716c2e897f5d1414f
Alvian N R <42503273+lambnet@users.noreply.github.com>
2022-05-05 10:35:16 +0800

    add category modal

--- pages/category/index.js
+++ pages/category/index.js
@@ -2,11 +2,35 @@ import React, { useEffect, useState } from 'react';
 import Layout from '@components/Layout';
 import Link from 'next/link';
 import Search from 'antd/lib/input/Search';
-import { Modal, Table } from 'antd';
+import { Button,Modal, Table } from 'antd';
 import {UserAddOutlined, EditOutlined, DeleteOutlined} from '@ant-design/icons';
 
 export default function Index() {
-  const [categoryData, setCategoryData] = useState([])
+  // state for modal
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 1000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setVisible(false);
+  }
+
+
+
+
+  const [categoryData, setCategoryData] = useState([]);
   useEffect(() => {
     const sortedData = data.sort((a, b) => a.name.localeCompare(b.name));
     const numberedData = sortedData.map((item, index) => ({
@@ -40,12 +64,12 @@ export default function Index() {
       render: (t, r) => 
         <div className='place-content-center'>
           <Link href={'/user/detail/'+r.id}>
-              <a className=" text-center px-4 pb-1 rounded-md text-white bg-blue-600 hover:bg-transparent border-2 border-blue-600 hover:text-blue-600">            
+              <a className="float-left text-center px-4 pb-1 rounded-md text-white bg-blue-600 hover:bg-transparent border-2 border-blue-600 hover:text-blue-600">            
                 <EditOutlined /> Edit
               </a>
           </Link>
 
-          <a onClick={() => deleteUser(r.id)} className="text-center inline px-3 pb-1 rounded-md text-white bg-red-800 hover:bg-transparent border-2 border-red-800 hover:text-red-800">
+          <a onClick={() => deleteUser(r.id)} className="float-right text-center inline px-3 pb-1 rounded-md text-white bg-red-800 hover:bg-transparent border-2 border-red-800 hover:text-red-800">
             <DeleteOutlined /> Delete
           </a>
         </div>
@@ -73,6 +97,23 @@ export default function Index() {
   
   function onChange(pagination, filters, sorter, extra) {
     console.log('params', pagination, filters, sorter, extra);
+    let filteredData = categoryData;
+    // sort filterdata by sorter.field asc
+    if (sorter.field) {
+      filteredData = filteredData.sort((a, b) => {
+        if (sorter.order === 'ascend') {
+          return a[sorter.field] - b[sorter.field];
+        } else {
+          return b[sorter.field] - a[sorter.field];
+        }
+      }
+      );
+    }
+    const numberedFilteredData = filteredData.map((item, index) => ({
+      ...item,
+      numrow: index + 1,
+    }));
+    setCategoryData(numberedFilteredData);
   }
 
   const filterData = (e) => {
@@ -96,11 +137,18 @@ export default function Index() {
   return (
     <Layout title="Category" subtitle="">
       <div className='w-[200px] float-left'>
-        <Link href="user/form">
-          <a className='text-center px-4 pb-2 pt-1 rounded-md text-white bg-green-600 hover:bg-transparent border-2 border-green-600 hover:text-green-600'>
-            <UserAddOutlined /> Add Category
-          </a>
-        </Link>
+        <Button type='primary' onClick={showModal}>
+          Add Category
+        </Button>
+        <Modal
+          title="Add Category"
+          visible={visible}
+          onOk={handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={handleCancel}
+        >
+          <p>Modal</p>
+        </Modal>
       </div>
       <div className='w-[200px] float-right'>
         <Search onChange={filterData} />
