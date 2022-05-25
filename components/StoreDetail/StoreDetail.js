f50edf7b30c2832758481de02b01af6f307ca462
augioka <putuaugiokaadiana@gmail.com>
2022-05-25 12:58:58 +0700

    add employee (modal, get list of employee by roles)

--- components/StoreDetail/StoreDetail.js
+++ components/StoreDetail/StoreDetail.js
@@ -11,8 +11,13 @@ import {
 	Tag,
 	Input,
 	message,
+	Modal,
+	Form,
+	Select,
 } from 'antd'
+import { useForm } from 'antd/lib/form/Form'
 import * as api from 'api/Store'
+import * as role from 'api/Role'
 import { useEffect, useState } from 'react'
 const { Search } = Input
 
@@ -27,12 +32,16 @@ const StoreDetail = ({ storeId }) => {
 	const [employee, setEmployee] = useState(null)
 	const [loading, setLoading] = useState({ store: true, employee: true })
 
+	const [visible, setVisible] = useState(false)
+
 	const [page, setPage] = useState(1)
 	const [pageSize, setPageSize] = useState(10)
 	const [serachValue, setSearchValue] = useState('')
 	const [sortBy, setSortBy] = useState('')
 	const [sortDir, setSortDir] = useState('')
 	const [totalPage, setTotalPage] = useState(1)
+
+	const [form] = useForm()
 
 	useEffect(() => {
 		if (storeId) {
@@ -110,6 +119,38 @@ const StoreDetail = ({ storeId }) => {
 					return { ...curr, employee: false }
 				})
 			})
+	}
+
+	const onSave = (value) => {
+		// if (mode == 'Create') {
+		// 	create(value)
+		// 		.then((res) => {
+		// 			if (res) {
+		// 				message.success(res.data.message)
+		// 				fetchStore()
+		// 			}
+		// 		})
+		// 		.catch((err) => {
+		// 			if (err) {
+		// 				message.error(err.response.data.message)
+		// 			}
+		// 		})
+		// } else {
+		// 	update(storeId, value)
+		// 		.then((res) => {
+		// 			if (res) {
+		// 				message.success(res.data.message)
+		// 				fetchStore()
+		// 			}
+		// 		})
+		// 		.catch((err) => {
+		// 			if (err) {
+		// 				message.error(err.response.data.message)
+		// 			}
+		// 		})
+		// }
+		console.log('Received values of form: ', value)
+		setVisible(false)
 	}
 
 	const onSortAndPagination = (pagination, sorter) => {
@@ -226,6 +267,101 @@ const StoreDetail = ({ storeId }) => {
 		},
 	]
 
+	const AddEmployeeModal = () => {
+		const [employeeOptionData, setEmployeeOptionData] = useState(null)
+		const [loadingEmployeeOption, setLoadingEmployeeOption] = useState(true)
+
+		const fetchEmployeeByRole = (roleId) => {
+			role.getRoleById(roleId).then((res) => {
+				if (res) {
+					setEmployeeOptionData(res.result)
+					setLoadingEmployeeOption(false)
+				} else {
+					message.error('Failed To Get Employees List')
+				}
+			})
+		}
+
+		return (
+			<Modal
+				visible={visible}
+				title={`Add Employee to the Store`}
+				okText="Save"
+				cancelText="Cancel"
+				onCancel={() => {
+					form.resetFields()
+					setVisible(false)
+					setEmployeeOptionData(null)
+				}}
+				onOk={() => {
+					form
+						.validateFields()
+						.then((values) => {
+							form.resetFields()
+							onSave(values)
+							setEmployeeOptionData(null)
+						})
+						.catch((info) => {
+							console.log('Validate Failed:', info)
+						})
+				}}
+			>
+				<Form form={form} layout="vertical" name="add_employee_fom">
+					<Form.Item
+						label="Select Role"
+						name="role"
+						rules={[
+							{ required: true, message: 'Please select employee role' },
+						]}
+					>
+						<Select onSelect={fetchEmployeeByRole}>
+							<Select.Option value="4">Stockist</Select.Option>
+							<Select.Option value="5">Cashier</Select.Option>
+						</Select>
+					</Form.Item>
+					<Form.Item
+						label="Select Employee"
+						name="userId"
+						rules={[
+							{ required: true, message: 'Please select new manager' },
+						]}
+					>
+						<Select
+							showSearch
+							loading={loadingEmployeeOption}
+							filterOption={(input, option) =>
+								option.children
+									.toLowerCase()
+									.indexOf(input.toLowerCase()) >= 0
+							}
+							filterSort={(optionA, optionB) =>
+								optionA.children
+									.toLowerCase()
+									.localeCompare(optionB.children.toLowerCase())
+							}
+						>
+							{employeeOptionData
+								? employeeOptionData.users.map((item) => {
+										return (
+											<Select.Option
+												value={item.id}
+												key={item.id}
+												disabled={item.workAt}
+											>
+												{item.workAt && item.workAt.length > 0
+													? `${item.firstName} ${item.lastName} (Work at ${item.workAt[0].store.name})`
+													: `${item.firstName} ${item.lastName}`}
+											</Select.Option>
+										)
+								  })
+								: null}
+						</Select>
+					</Form.Item>
+				</Form>
+			</Modal>
+		)
+	}
+
 	return (
 		<>
 			<Divider orientation="left" orientationMargin={0}>
@@ -256,12 +392,11 @@ const StoreDetail = ({ storeId }) => {
 					<Button
 						type="primary"
 						onClick={() => {
-							setMode('Create')
 							setVisible(true)
 						}}
 						icon={<PlusOutlined />}
 					>
-						New store
+						Add Employee
 					</Button>
 				</Col>
 			</Row>
@@ -284,6 +419,7 @@ const StoreDetail = ({ storeId }) => {
 					onSortAndPagination(pagination, sorter)
 				}}
 			></Table>
+			<AddEmployeeModal />
 		</>
 	)
 }
